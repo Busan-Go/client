6ff4634c53a998a576cfa7e13e2e277b014ca1ce
KimCookieYa <min49590@gmail.com>
2023-11-10 00:28:23 +0900

    Feat: MissionPlace

--- screens/MissionPlaceScreen.jsx
+++ screens/MissionPlaceScreen.jsx
@@ -1,7 +1,26 @@
-import { ScrollView, StyleSheet, View, Text, Pressable } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { TextInput } from "react-native-paper";
+import { useEffect, useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function MissionPlaceScreen() {
+  const [text, setText] = useState("");
+
+  const clearInput = () => {
+    setText("");
+  };
+
+  useEffect(() => {
+    // db에서 관광지 검색
+    console.log(text);
+  }, [text]);
+
   return (
     <View style={styles.container}>
       <ScrollView
@@ -16,7 +35,24 @@ export default function MissionPlaceScreen() {
 
           <Text style={styles.title}>{process.env.EXPO_PUBLIC_APP_TITLE}</Text>
           <Text style={styles.title}>미션 장소</Text>
-
+          <Text style={styles.description}>
+            대이로그는 총 246개의 미션장소를 제공합니다. 미션을 수행하고 싶은
+            곳을 검색해보세요.
+          </Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              onChangeText={setText}
+              value={text}
+              placeholder="Type place.."
+              underlineColor="white"
+            />
+            {text ? (
+              <TouchableOpacity onPress={clearInput} style={styles.clearButton}>
+                <Ionicons name="md-close-circle" size={30} color="lightgray" />
+              </TouchableOpacity>
+            ) : null}
+          </View>
           <View style={{ width: "100%", height: 100 }}></View>
         </View>
       </ScrollView>
@@ -44,5 +80,22 @@ const styles = StyleSheet.create({
   },
   description: {
     color: "gray",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 50,
+    overflow: "hidden",
+    margin: 10,
+  },
+  input: {
+    flex: 1,
+    padding: 8,
+    height: 40,
+    backgroundColor: "white",
+  },
+  clearButton: {
+    position: "absolute",
+    right: 10,
   },
 });
